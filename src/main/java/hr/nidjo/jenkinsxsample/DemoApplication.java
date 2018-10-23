e1d25140ddf400512867552b33815a57a7343322
nikolagereci <37083905+nikolagereci@users.noreply.github.com>
2018-10-23 10:53:58 +0200

    comment added

--- src/main/java/hr/nidjo/jenkinsxsample/DemoApplication.java
+++ src/main/java/hr/nidjo/jenkinsxsample/DemoApplication.java
@@ -2,6 +2,7 @@ package hr.nidjo.jenkinsxsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*comment*/
 
 @SpringBootApplication
 public class DemoApplication {
